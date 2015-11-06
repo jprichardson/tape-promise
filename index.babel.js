a8b7f47c3084c575284a57dfe64fbb86087c10c3
JP Richardson <jprichardson@gmail.com>
2015-11-06 07:56:59 -0600

    babel added

---
+++ index.babel.js
@@ -0,0 +1,26 @@
+import onetime from 'onetime'
+
+export default function tapePromiseFactory (tapeTest) {
+  function testPromise (description, testFn) {
+    tapeTest(description, function (t) {
+      t.end = onetime(t.end)
+      process.once('unhandledRejection', t.end)
+      var p
+      try {
+        p = testFn(t)
+        if (p && p.then && typeof p.then === 'function') {
+          p.then(t.end).catch(t.end)
+        }
+      } catch (e) {
+        t.end(e)
+      }
+    })
+  }
+
+  Object.keys(tapeTest).forEach((key) => {
+    if (typeof tapeTest[key] !== 'function') return
+    testPromise[key] = tapeTest[key]
+  })
+
+  return testPromise
+}
---
+++ index.es5.js
@@ -0,0 +1,39 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports['default'] = tapePromiseFactory;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _onetime = require('onetime');
+
+var _onetime2 = _interopRequireDefault(_onetime);
+
+function tapePromiseFactory(tapeTest) {
+  function testPromise(description, testFn) {
+    tapeTest(description, function (t) {
+      t.end = (0, _onetime2['default'])(t.end);
+      process.once('unhandledRejection', t.end);
+      var p;
+      try {
+        p = testFn(t);
+        if (p && p.then && typeof p.then === 'function') {
+          p.then(t.end)['catch'](t.end);
+        }
+      } catch (e) {
+        t.end(e);
+      }
+    });
+  }
+
+  Object.keys(tapeTest).forEach(function (key) {
+    if (typeof tapeTest[key] !== 'function') return;
+    testPromise[key] = tapeTest[key];
+  });
+
+  return testPromise;
+}
+
+module.exports = exports['default'];
